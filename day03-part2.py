c71bd9aa4e29790ca47bc3c7a13b83c206b26162
pavelb <pavelbakhilau@gmail.com>
2023-12-02 21:45:11 -0800

    Deleting dead code

--- day03-part2.py
+++ day03-part2.py
@@ -6,12 +6,11 @@ with open('day03-input.txt', 'r') as input:
   lines = list(line.rstrip() for line in input)
   for i, line in enumerate(lines):
     for m in re.finditer(r'\d+', line):
-      touchingSymbol = False
       for j in range(max(0, i - 1), min(len(lines), i + 2)):
         checkLine = lines[j]
         for k in range(max(0, m.start() - 1), min(len(line), m.end() + 1)):
           if checkLine[k] == '*':
-            mem[(j, k)].append(int(m.group()))
+            mem[j, k].append(int(m.group()))
 
 total = 0
 for nums in mem.values():
