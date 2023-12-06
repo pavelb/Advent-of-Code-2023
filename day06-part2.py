841672c0815b10baa8fb34dc8a0fa241285db4f2
pavelb <pavelbakhilau@gmail.com>
2023-12-05 21:44:07 -0800

    Fix a technical logic bug in the code that the input does not reveal

--- day06-part2.py
+++ day06-part2.py
@@ -1,7 +1,9 @@
+from math import sqrt, ceil, floor
+
 with open('day06-input.txt', 'r') as input:
   lines = list(input)
   t = int(lines[0].split(" ", 1)[1].replace(' ', ''))
   d = int(lines[1].split(" ", 1)[1].replace(' ', ''))
-  w1 = (t - (t*t - 4 * d) ** .5) // 2
-  w2 = (t + (t*t - 4 * d) ** .5) // 2
-  print(int(w2 - w1))
+  w1 = ceil((t - sqrt(t*t - 4 * d)) / 2)
+  w2 = floor((t + sqrt(t*t - 4 * d)) / 2)
+  print(w2 - w1 + 1)
