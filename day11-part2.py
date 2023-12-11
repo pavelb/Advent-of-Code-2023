9822eafa028dcff070ad35f485814f663dd03e8a
pavelb <pavelbakhilau@gmail.com>
2023-12-10 22:41:40 -0800

    Improve day 11

--- day11-part2.py
+++ day11-part2.py
@@ -2,13 +2,7 @@ with open('day11-input.txt', 'r') as input:
   map = [line.rstrip() for line in input]
   rowDelta = [1 if '#' in row else 1000000 for row in map]
   colDelta = [1 if any(row[x] == '#' for row in map) else 1000000 for x in range(len(map[0]))]
-
-  galaxies = []
-  for y, row in enumerate(map):
-    for x, c in enumerate(row):
-      if c == '#':
-        galaxies.append((x, y))
-  
+  galaxies = [(x, y) for y, row in enumerate(map) for x, c in enumerate(row) if c == '#']
   total = 0
   for i, (x1, y1) in enumerate(galaxies):
     for j in range(i + 1, len(galaxies)):
