0cada03a2f94ac2a2a12027042c773d6078d20cf
pavelb <pavelbakhilau@gmail.com>
2023-12-11 22:14:31 -0800

    Day 12

---
+++ day12-part2.py
@@ -0,0 +1,28 @@
+from functools import cache
+
+@cache
+def satisfy(condition, springs):
+  if len(condition) < sum(springs):
+    return 0
+
+  out = 0
+  if condition[0] != '#':
+    out += satisfy(condition[1:], springs)
+  n = springs[0]
+  if '.' not in condition[:n]:
+    if len(springs) == 1:
+      out += 1 if '#' not in condition[n:] else 0
+    elif len(condition) > n and condition[n] != '#':
+      out += satisfy(condition[n + 1:], springs[1:])
+  return out
+
+with open('day12-input.txt', 'r') as input:
+  total = 0
+  for line in input:
+    line = line.rstrip()
+    condition, springs = line.split(" ")
+    condition = '?'.join([condition] * 5)
+    springs = ','.join([springs] * 5)
+    springs = list(map(int, springs.split(",")))
+    total += satisfy(condition, tuple(springs))
+  print(total)
