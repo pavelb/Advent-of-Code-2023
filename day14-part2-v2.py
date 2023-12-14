730e189b844ade197ff3a059a4981d1511fb03d9
pavelb <pavelbakhilau@gmail.com>
2023-12-14 01:36:51 -0800

    Update day14-part2-v2.py

--- day14-part2-v2.py
+++ day14-part2-v2.py
@@ -1,6 +1,7 @@
 from itertools import count, product
 
 def slide(board, dx, dy):
+  board = list(map(list, board))
   rx = range(len(board[0]))
   if dx > 0:
     rx = reversed(rx)
@@ -14,24 +15,24 @@ def slide(board, dx, dy):
         x += dx
         y += dy
         board[y][x] = 'O'
+  return tuple(map(tuple, board))
 
 with open('day14-input.txt', 'r') as input:
-  board = [list(line.rstrip()) for line in input]
+  board = tuple(tuple(line.rstrip()) for line in input)
   cyclesToBoard = dict()
   boardToCycles = dict()
   for i in range(1_000_000_000):
-    key = tuple(map(tuple, board))
-    if key in boardToCycles:
-      loopStart = boardToCycles[key]
+    if board in boardToCycles:
+      loopStart = boardToCycles[board]
       loopLen = i - loopStart
       board = cyclesToBoard[loopStart + (1_000_000_000 - loopStart) % loopLen]
       break
-    cyclesToBoard[i] = key
-    boardToCycles[key] = i
-    slide(board, 0, -1)
-    slide(board, -1, 0)
-    slide(board, 0, 1)
-    slide(board, 1, 0)
+    cyclesToBoard[i] = board
+    boardToCycles[board] = i
+    board = slide(board, 0, -1)
+    board = slide(board, -1, 0)
+    board = slide(board, 0, 1)
+    board = slide(board, 1, 0)
 
   total = 0
   for y, row in enumerate(board):
